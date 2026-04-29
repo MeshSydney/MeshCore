52bc0c0f9c36a8b40a4c24b851da65b1c21e9094
overkillfpv <t.hitchins@exemail.com.au>
2026-04-29 19:13:35 +1000

    fix(MyMesh): remove redundant initContacts call in begin method for simple repeater and room server

--- examples/simple_room_server/MyMesh.cpp
+++ examples/simple_room_server/MyMesh.cpp
@@ -705,7 +705,6 @@ MyMesh::MyMesh(mesh::MainBoard &board, mesh::Radio &radio, mesh::MillisecondCloc
 
 void MyMesh::begin(FILESYSTEM *fs) {
   mesh::Mesh::begin();
-  initContacts();
   _fs = fs;
   // load persisted prefs
   _cli.loadPrefs(_fs);
